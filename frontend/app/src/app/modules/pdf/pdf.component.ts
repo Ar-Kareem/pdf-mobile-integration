f7922f56362fabea7c91927477b13fcd04ed0af7
Ar-Kareem <abdulrahman_3000@hotmail.com>
2021-10-29 23:24:05 +0300

    fix pdf to fit exactly on screen without extra scrollbars

--- frontend/app/src/app/modules/pdf/pdf.component.ts
+++ frontend/app/src/app/modules/pdf/pdf.component.ts
@@ -1,4 +1,7 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { authActions, authSelectors } from '@modules/auth/auth.reducer';
+import { Store } from '@ngrx/store';
+import { getDefaultApplicationManifest, setGlobalApplicationManifest } from 'src/app/app.manifest';
 import { PdfService } from './pdf.service';
 
 @Component({
@@ -16,21 +19,44 @@ export class PdfComponent implements OnInit {
     available: true,
   }
 
+  toolbarOpen = true;
+
   constructor(
+    private store: Store,
     private pdfService: PdfService,
     private changeDetectorRef: ChangeDetectorRef
   ) { }
 
   ngOnInit(): void {
     (window as any)['PdfComponent'] = this
+    this.setupManifest();
+    this.initStore();
   }
 
-  async download() {
+  private initStore() {
+    this.store.select(authSelectors.selectHeaderVisibility).subscribe(status => {this.toolbarOpen = status})
+  }
+
+  onClickPdfViewer(event: MouseEvent) {
+    if (event.detail % 3 == 0) {
+      this.store.dispatch(authActions.toggleHeaderVisibility());
+    }
+  }
+
+  private setupManifest() {
+    const manifestJSON = getDefaultApplicationManifest();
+    manifestJSON['background_color'] = '#888888'
+    manifestJSON['theme_color'] = '#888888'
+    setGlobalApplicationManifest(manifestJSON)
+  }
+
+
+  private async download() {
     const result = await this.pdfService.download('https://arxiv.org/pdf/1905.11397.pdf').toPromise()
     console.log(result);
   }
 
-  async last_saved() {
+  private async last_saved() {
     const result = await this.pdfService.last_saved().toPromise()
     this.pdf.src = result.last_saved_pdf
     this.pdf.available = true;
