a5b9de41078213374799da90b12d658c82440205
Ar-Kareem <abdulrahman_3000@hotmail.com>
2021-11-05 23:05:56 +0300

    add helper function in base_mapper

--- backend/app/base/base_mapper.py
+++ backend/app/base/base_mapper.py
@@ -1,19 +1,27 @@
 """Contains useful basic function to interact with the database."""
+import logging
 from pathlib import Path
 import sqlite3
 from sqlite3.dbapi2 import Connection
 from flask import g
 
+logger = logging.getLogger(__name__)
+
 
 def get_db() -> Connection:
     """Get database object from flask, if not exist, will initiate a new connection and assign it to flask globals."""
     if "db" not in g:
-        g.db = sqlite3.connect("/app/database/main.db")
-        g.db.row_factory = sqlite3.Row
+        g.db = _get_specific_db_version(_CURRENT_DB_VERSION)
     return g.db
 
 
-def close_db(exception):
+def _get_specific_db_version(version):
+    db = sqlite3.connect(f"/app/database/main-v{version}.db")
+    db.row_factory = sqlite3.Row
+    return db
+
+
+def close_db():
     """Remove database from flask globals if exist and close the connection."""
     db = g.pop("db", None)
     if db is not None:
