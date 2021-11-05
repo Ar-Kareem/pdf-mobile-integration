189c21de4764e2b192b1536d828b8710bc3d1c66
Ar-Kareem <abdulrahman_3000@hotmail.com>
2021-11-05 23:06:26 +0300

    implement basic support for DB upgrading

--- backend/app/base/base_mapper.py
+++ backend/app/base/base_mapper.py
@@ -1,11 +1,15 @@
 """Contains useful basic function to interact with the database."""
 import logging
+import os
+import re
 from pathlib import Path
 import sqlite3
 from sqlite3.dbapi2 import Connection
 from flask import g
 
 logger = logging.getLogger(__name__)
+
+_CURRENT_DB_VERSION = 1
 
 
 def get_db() -> Connection:
@@ -29,18 +33,67 @@ def close_db():
 
 
 def init_db():
-    """Init database, if doesn't exist, will create one."""
+    """Init database, if doesn't exist, will create one and upgrade from previous versions if available."""
     Path("/app/database").mkdir(exist_ok=True)
-    db = get_db()
+    db_versions = [re.findall('v(.+)\\.', f) for f in os.listdir(Path('/app/database'))]
+    db_versions = [int(v[0]) for v in db_versions if len(v) > 0]
 
-    sql = '''
-    CREATE TABLE IF NOT EXISTS users (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            gid TEXT NOT NULL UNIQUE,
-            first_name TEXT NOT NULL,
-            last_name TEXT NOT NULL,
-            email TEXT UNIQUE NOT NULL,
-            picture TEXT
-        );
-    '''
+    if len(db_versions) == 0:  # no db exists
+        logger.info('Creating fresh db')
+        db = get_db()
+        _create_tables(db, _CURRENT_DB_VERSION)
+    else:  # 1 or more db exists
+        if max(db_versions) > _CURRENT_DB_VERSION:
+            logger.error('Database Exists on file that is larger than the current app DB version. This should not be possible')
+        if _CURRENT_DB_VERSION not in db_versions:
+            db_versions = [v for v in db_versions if v <= _CURRENT_DB_VERSION]  # filter versions larger than current just in case
+            _upgrade_db(from_version=max(db_versions), to_version=_CURRENT_DB_VERSION)
+
+
+def _create_tables(db, version):
+    if version == 1:
+        sql = '''
+            CREATE TABLE IF NOT EXISTS users (
+                    id INTEGER PRIMARY KEY,
+                    gid TEXT NOT NULL UNIQUE,
+                    first_name TEXT NOT NULL,
+                    last_name TEXT NOT NULL,
+                    email TEXT UNIQUE NOT NULL,
+                    picture TEXT
+                );
+        '''
+    else:
+        sql = ''
+        logger.critical(f'Do not know how to create tables for version {version}.')
+        raise ValueError(f'Do not know how to create tables for version {version}.')
+
     db.executescript(sql)
+
+
+def _upgrade_db(from_version: int, to_version: int):
+    if from_version >= to_version:
+        logger.error(f'from_version >= to_version, should not happen. {from_version}>={to_version}')
+        return
+    if (to_version - from_version) >= 2:  # need to upgrade more than one version
+        for ver in range(from_version, to_version):
+            _upgrade_db(ver, ver + 1)
+        return
+    logger.info(f'Upgrading from version {from_version} to {to_version}...')
+
+    db_from = _get_specific_db_version(from_version)
+    db_to = _get_specific_db_version(to_version)
+    _create_tables(db_to, to_version)
+
+    if from_version == 1 and to_version == 2:
+        # users = db_from.execute('SELECT id, gid, first_name, last_name, email, picture FROM users').fetchall()
+        # for u in users:
+        #     db_to.execute(
+        #         """INSERT INTO
+        #             users (id, gid, first_name, last_name, email, picture)
+        #             VALUES (?, ?, ?, ?, ?, ?)
+        #         """,
+        #         (u[0], u[1], u[2], u[3], u[4], u[5]),
+        #     )
+        db_to.commit()
+    else:
+        logger.error(f'Do not know how to upgrade from version {from_version} to {to_version}.')
