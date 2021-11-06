f93e22c78939ebe882c3f5883e3ef57b280a7afe
Ar-Kareem <abdulrahman_3000@hotmail.com>
2021-11-06 14:40:04 +0300

    add defalut menu bar to sidebar

--- frontend/app/src/app/app.module.ts
+++ frontend/app/src/app/app.module.ts
@@ -22,6 +22,7 @@ import { FormsModule } from '@angular/forms';
 import { pdfFeatureKey, pdfReducer } from '@modules/pdf/pdf.reducer';
 import { HttpErrorInterceptor } from '@services/HttpErrorInterceptor';
 import { PdfEffects } from '@modules/pdf/pdf.effects';
+import { PanelMenuModule } from 'primeng/panelmenu';
 
 @NgModule({
   declarations: [
@@ -47,6 +48,7 @@ import { PdfEffects } from '@modules/pdf/pdf.effects';
     AppRoutingModule,
     ButtonModule,
     SidebarModule,
+    PanelMenuModule,
     PdfViewerModule,
   ],
   providers: [
